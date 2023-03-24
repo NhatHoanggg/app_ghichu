5039dd7bc9ba895e66840b83388ccf6761827410
Nhat Hoang <103094181+NhatHoanggg@users.noreply.github.com>
2023-03-24 12:55:02 +0700

    a

---
+++ ghichu/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    ENTER NOTE
+    <input type="text" v-model="currentNote">
+    <button @click = "save">SAVE</button>
+    <hr>
+      List Note: 
+    <div v-for = "(item, index) of list" :key = "index">
+      <input type="checkbox" v-model="item.done">
+      <span class = "{done: item.done}">
+        {{ item.content }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      currentNote: "",
+      
+      list : [
+        {content: 'ngu', done: false},
+        {content: 'an', done: false},
+        {content: 'di choi', done: false},
+        {content: 'hoc bai', done: false},
+        {content: 'xem phim', done: false},
+      ],
+    }
+  },
+  mounted(){
+
+  },
+  computed: {
+
+  },
+
+  methods:{
+    save(){
+      //alert(this.currentNote);
+      //this.list.push(this.currentNote)
+      this.list.push({content: this.currentNote, done: false})
+    }
+  }
+}
+</script>
+
+<style>
+.done{
+  /* text-decoration: line-through; */
+  color: blue;
+}
+</style>
