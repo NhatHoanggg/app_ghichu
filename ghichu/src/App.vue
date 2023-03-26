bfe8d10a5716d17626c5ddd65f608dfc4774010e
Nhat Hoang <103094181+NhatHoanggg@users.noreply.github.com>
2023-03-26 19:00:59 +0700

    add new project app_ghichu

--- ghichu/src/App.vue
+++ ghichu/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    ENTER NOTE
-    <input type="text" v-model="currentNote">
-    <button @click = "save">SAVE</button>
+  <div class = "container">
+    <div class="header">
+      <div>
+        ENTER TASKS
+        <input class = "input-box" type="text" v-model="currentNote" placeholder="Input tasks here..">
+        <button class = "btn" @click = "save">SAVE</button>
+      </div>
+      you are typing: {{currentNote}}
     <hr>
       List Note: 
-    <div v-for = "(item, index) of list" :key = "index">
-      <input type="checkbox" v-model="item.done">
-      <span class = "{done: item.done}">
-        {{ item.content }}
+    </div>
+    <div v-for = "(item, index) of list" :key = "index" class = "item">
+      <input type="checkbox" v-model="item.done" >
+      <span class = "content" v-bind:class = "{done: item.done}" >
+        {{ item.content }} 
       </span>
     </div>
   </div>
@@ -20,7 +25,6 @@ export default {
   data(){
     return{
       currentNote: "",
-      
       list : [
         {content: 'ngu', done: false},
         {content: 'an', done: false},
@@ -39,8 +43,6 @@ export default {
 
   methods:{
     save(){
-      //alert(this.currentNote);
-      //this.list.push(this.currentNote)
       this.list.push({content: this.currentNote, done: false})
     }
   }
@@ -48,8 +50,61 @@ export default {
 </script>
 
 <style>
-.done{
-  /* text-decoration: line-through; */
-  color: blue;
+*{
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
+
+.done{
+  text-decoration: line-through;
+}
+
+.container{
+  width: 100%;
+  height: 1000px;
+  font-size: 30px;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  
+}
+
+.header{
+  text-align: center;
+}
+
+.input-box{
+  font-size: 30px;
+  margin-left: 30px;
+}
+
+.btn{
+  font-size: 30px;
+  margin-left: 30px;
+}
+
+.content{
+  margin-left: 15px;
+}
+
+.item{
+  width: 800px;
+  padding: 15px;
+  margin: 0 ;
+  margin-left: 15px;
+  border: 1px solid #ddd; 
+  background-color: #f6f6f6; 
+  font-size: 30px; 
+  color: black; 
+  display: block;
+  transform: translateX(40%);
+}
+
+.item:hover{
+  background-color: #eee;
+  font-size: larger;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+
 </style>
+
